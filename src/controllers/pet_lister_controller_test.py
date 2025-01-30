fd45e8bd5f58ab68908e06de06782dacf46899b4
guimileib <guilhermemileib@gmail.com>
2025-01-30 15:27:33 -0300

    feat: Implementing pet_deleter_cotroller with tests

--- src/controllers/pet_lister_controller_test.py
+++ src/controllers/pet_lister_controller_test.py
@@ -4,8 +4,8 @@ from .pet_lister_controller import PetListerController
 class MockPetsRepository:
     def list_pets(self):
         return [
-            PetsTable(name="Buddy", type="Dog", id=47).to_dict(),
-            PetsTable(name="Fluffy", type="Cat", id=4).to_dict()
+            PetsTable(name="Buddy", type="Dog", id=47),
+            PetsTable(name="Fluffy", type="Cat", id=4),
         ]
     
 def test_list_pets():
@@ -18,7 +18,7 @@ def test_list_pets():
             "count": 2,
             "attributes": [
                 {"name": "Buddy", "id": 47},
-                {"name": "Fluffy", "id": 4}
+                {"name": "Fluffy", "id": 4},
             ]
         }
     }
