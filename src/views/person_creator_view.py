df854bafc34f0bf8f4f2a36448f0e0719eb573b8
guimileib <guilhermemileib@gmail.com>
2025-01-30 21:21:52 -0300

    feat: Implementing person_creator_view and creating test

---
+++ src/views/person_creator_view.py
@@ -0,0 +1,16 @@
+#pylint: disable=missing-final-newline
+from src.controllers.person_creator_controller import PersonCreatorController
+from .http_types.http_request import HttpRequest
+from .http_types.http_response import HttpResponse
+from .interfaces.view_interface import ViewInterface
+
+class PersonCreatorView(ViewInterface):
+    def __init__(self, controller: PersonCreatorController) -> None:
+        self.__controller = controller 
+
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        person_info = http_request.body
+        body_reponse = self.__controller.create(person_info)
+        
+        return HttpResponse(status_code=201, body=body_reponse)
+    
